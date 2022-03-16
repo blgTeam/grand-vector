e04064a2fed04edb9c56ab9ccf3c8417e0e02aa8
blgTeam <squad@bestlyfegroup.com>
2022-03-16 15:47:43 -0400

    html added jason

---
+++ assets/footer.php
@@ -0,0 +1,33 @@
+      <!-- Page Footer -->
+      <footer class="page-foot bg-light">
+        <section class="footer-content">
+          <div class="container">
+            <div class="rd-navbar-brand"><a class="brand-name" href="index.html"><svg class="text-logo" width="130px" height="36px" viewBox="0 0 130 36" shape-rendering="geometricPrecision">
+<text transform="matrix(1 0 0 1 -0.5197 33.3432)" font-family="Pacifico" font-size="36px">Vector</text>
+<text transform="matrix(1 0 0 1 68 11.125)" font-family="Montserrat" font-size="14" letter-spacing="1">GRAND</text>
+<path class="logo" fill-rule="evenodd" clip-rule="evenodd" stroke-linecap="round"  d="M95.313,25.564c9.022-6.656,20.032-9.736,30-5"/>
+</svg></a></div>
+            <ul class="list-inline">
+              <li><a class="fa-facebook" href="#"></a></li>
+              <li><a class="fa-twitter" href="#"></a></li>
+              <li><a class="fa-pinterest-p" href="#"></a></li>
+              <li><a class="fa-vimeo" href="#"></a></li>
+              <li><a class="fa-google" href="#"></a></li>
+              <li><a class="fa-rss" href="#"></a></li>
+            </ul>
+          </div>
+        </section>
+        <section class="footer-copyright-1 copyright">
+          <div class="container">
+            <p>&#169; <span class="copyright-year"></span> All Rights Reserved <a href="terms.html">Terms of Use and Privacy Policy</a></p>
+          </div>
+        </section>
+      </footer>
+      <!-- Rd Mailform result field-->
+      <div class="rd-mailform-validate"></div>
+    </div>
+    <!-- Global Mailform Output-->
+    <div class="snackbars" id="form-output-global"></div>
+    <!-- Java script-->
+    <script src="js/core.min.js"></script>
+    <script src="js/script.js"></script>
